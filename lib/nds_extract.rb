94295da30ff9de35c59e86e2123d7dabf61c2f3d
José Romero <jdromerov88@gmail.com>
2019-10-25 23:29:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -84,13 +84,13 @@ def gross_per_studio(collection)
   #studio_name = collection[0][:studio]
   i = 0
   while i < collection.length do
-  total += collection[i][:worldwide_gross]
-  studio_name = collection[i][:studio]
-  new_hash_with_gross_total = {
-    studio_name: studio_name,
-    total_sum: total
-  }
-  i += 1
+    total += collection[i][:worldwide_gross]
+    studio_name = collection[i][:studio]
+    new_hash_with_gross_total = {
+      studio_name: studio_name,
+      total_sum: total
+    }
+    i += 1
   end
   #puts total
   puts new_hash_with_gross_total
