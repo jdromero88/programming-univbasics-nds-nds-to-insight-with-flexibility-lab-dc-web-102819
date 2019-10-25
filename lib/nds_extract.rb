c09cd6c7a5789e4004f84cae2e202abd0ccd4faa
José Romero <jdromerov88@gmail.com>
2019-10-25 23:07:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -79,7 +79,7 @@ def gross_per_studio(collection)
   # total of all the worldwide_gross numbers for every movie in the input Hash
   pp collection
   new_hash_with_gross_total = {}
-  total = collection[:worldwide_gross]
+  total = collection[:worldwide_gross].to_s
   puts total
 end
 
