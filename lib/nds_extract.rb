d85c7e286d62de06a9d4009d2c3bd9c2d8604388
José Romero <jdromerov88@gmail.com>
2019-10-25 23:26:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -92,7 +92,8 @@ def gross_per_studio(collection)
   i += 1
   end
   #puts total
-  puts new_hash_with_gross_total
+  #puts new_hash_with_gross_total
+  new_hash_with_gross_total
 end
 
 def movies_with_directors_set(source)
