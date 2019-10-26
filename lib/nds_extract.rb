8b3a87d2205dd2fc06cf0730c86d45651c075a53
José Romero <jdromerov88@gmail.com>
2019-10-26 00:51:34 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -90,7 +90,7 @@ def gross_per_studio(collection)
     studio_name = collection[i][:studio]
     while inner_count < collection.length do
       if studio_name == collection[inner_count][:studio]
-        total += collection[i][:worldwide_gross]
+        total += collection[inner_count][:worldwide_gross]
         puts "algo time #{inner_count}"
       end
       inner_count += 1
